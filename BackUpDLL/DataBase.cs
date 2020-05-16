32b1d7c31cec8e6541ccd26a1a62eceb053eef25
arad_enj <arad_enj@yahoo.com>
2020-05-16 19:52:35 +0430

    Sync

---
+++ BackUpDLL/DataBase.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using PacketParser.Services;
+using System.Windows.Forms;
+
+namespace BackUpDLL
+{
+    public class DataBase
+    {
+        public static bool BackUpStart(string connectionString, string fileLocation = "", bool autoBackUp = false, CancellationToken token = default)
+        {
+            try
+            {
+                token.ThrowIfCancellationRequested();
+                if (fileLocation == "")
+                {
+                    token.ThrowIfCancellationRequested();
+                    var dlg = new SaveFileDialog { Title = @"پشتیبان گیری اطلاعات نوین پرداز" };
+                    token.ThrowIfCancellationRequested();
+                    var file = Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly()?.Location)
+                        ?.Replace(".exe", "__");
+                    var d = Calendar.MiladiToShamsi(DateTime.Now).Replace("/", "_");
+                    d += "__" + DateTime.Now.Hour + " - " + DateTime.Now.Minute;
+                    file += d;
+                    file = file.Replace(" ", "");
+                    dlg.FileName = file;
+                    token.ThrowIfCancellationRequested();
+                    dlg.Filter = "*.Bak|*.Bak";
+                    token.ThrowIfCancellationRequested();
+                    string result;
+                    token.ThrowIfCancellationRequested();
+                    if (dlg.ShowDialog() != DialogResult.OK) return false;
+                    token.ThrowIfCancellationRequested();
+                    result = DatabaseAction.BackupDB(connectionString, dlg.FileName);
+                    token.ThrowIfCancellationRequested();
+
+                    if (result != "")
+                    {
+                        MessageBox.Show(@"پشتیبان گیری با موفقیت انجام شد", @"پیغام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+                        return true;
+                    }
+                    MessageBox.Show(@"خطا در عملیات پشتیبان گیری اطلاعات", @"پیغام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+                    return false;
+                }
+                else
+                {
+                    token.ThrowIfCancellationRequested();
+                    var result = DatabaseAction.BackupDB(connectionString, fileLocation);
+                    if (result != "")
+                    {
+                        token.ThrowIfCancellationRequested();
+                        if (autoBackUp == false) { MessageBox.Show("پشتیبان گیری با موفیقیت انجام شد", "پیغام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign); }
+
+                        return true;
+                    }
+                    token.ThrowIfCancellationRequested();
+                    if (autoBackUp == false) { MessageBox.Show("خطا در عملیات پشتیبان گیری اطلاعات", "پیغام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign); }
+
+                    return false;
+                }
+            }
+            catch (ThreadAbortException) { return false; }
+            catch (OperationCanceledException) { return false; }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                return false;
+            }
+        }
+        public static bool ReStoreStart(string connctionString, IWin32Window owner, string path = "", bool autoBackup = true)
+        {
+            try
+            {
+                if (path == "")
+                {
+                    var OFD = new OpenFileDialog();
+                    OFD.Multiselect = false;
+                    OFD.Title = @"فایل حاوی اطلاعات پشتیبانی نرم افزار را انتخاب نمائید";
+
+                    if (OFD.ShowDialog(owner) == DialogResult.OK)
+                    {
+                        var backUpVersion = GetBackUpVersion(connctionString, OFD.FileName);
+                        var dataBaseVersion = GetDataBaseVersion(connctionString);
+                        if (backUpVersion > dataBaseVersion)
+                        {
+                            MessageBox.Show($@"{backUpVersion} نسخه فایل پشتیبان" + " \r\n" +
+                                            $@"{dataBaseVersion} نسخه دیتابیس" + "\r\n" +
+                                            "بدلیل بالاتر بودن نسخه پشتیبان نسبت به دیتابیس، امکان بازگردانی وجود ندارد");
+                            return false;
+                        }
+
+                        SqlConnection.ClearAllPools();
+
+                        if (DatabaseAction.ReStoreDB(connctionString, OFD.FileName, autoBackup) == "")
+                        {
+
+                            MessageBox.Show("بازگردانی اطلاعات با موفقیت انجام شد", "پیغام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            var exepath = Path.Combine(Application.StartupPath, System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe");
+                            System.Diagnostics.Process.Start(exepath);
+                            return true;
+                        }
+                        else
+                        {
+                            MessageBox.Show(" خطا در بازگردانی اطلاعات ", "پیغام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            DatabaseAction.SetMultiUser(connctionString);
+                            return false;
+                        }
+
+                    }
+                    return false;
+
+                }
+                else
+                {
+                    string restoreMessage = DatabaseAction.ReStoreDB(connctionString, path, autoBackup);
+                    if (restoreMessage == "")
+                    {
+
+                        MessageBox.Show("بازگردانی اطلاعات با موفقیت انجام شد", "پیغام سیستم", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show(" خطا در بازگردانی اطلاعات " + Environment.NewLine + restoreMessage, "پیغام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                return false;
+            }
+        }
+
+        private static float GetBackUpVersion(string connctionString, string backUpFileName)
+        {
+            var ver = (float)0;
+            var line = 0;
+            try
+            {
+                var cn = new SqlConnection(connctionString);
+
+                line = 1;
+                line = 11;
+                if (string.IsNullOrEmpty(backUpFileName)) return ver;
+                line = 12;
+                var command = @"RESTORE HEADERONLY FROM DISK ='" + backUpFileName + "'";
+                line = 13;
+
+                using (var sqlCommand = new SqlCommand(command, cn))
+                {
+                    line = 14;
+                    cn.Open();
+                    line = 15;
+                    var sqlDataReader = sqlCommand.ExecuteReader();
+                    line = 16;
+                    while (sqlDataReader.Read())
+                    {
+                        line = 17;
+                        Console.WriteLine();
+                        line = 18;
+                        var d = $"{sqlDataReader["DatabaseVersion"]}";
+                        line = 19;
+                        if (string.IsNullOrEmpty(d)) continue;
+                        line = 20;
+                        switch (d)
+                        {
+                            case "406": ver = 6; break;
+                            case "408": ver = (float)6.5; break;
+                            case "515": ver = 7; break;
+                            case "539": ver = 2000; break;
+                            case "611": ver = 2005; break;
+                            case "612": ver = 2005; break;
+                            case "655": ver = 2008; break;
+                            case "661": ver = 2008; break;
+                            case "706": ver = 2012; break;
+                            case "782": ver = 2014; break;
+                            case "869": ver = 2017; break;
+                            case "895": ver = 2019; break;
+                            case "852": ver = 2016; break;
+                            default: ver = 0; break;
+                        }
+
+                        line = 21;
+                        return ver;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex,
+                    $"Error In Line{line} With ConnectionString:{connctionString} AND BackUpFileName:{backUpFileName}");
+                ver = 0;
+            }
+
+            return ver;
+        }
+
+        private static float GetDataBaseVersion(string connctionString)
+        {
+            var ver = (float)0;
+            try
+            {
+                var cn = new SqlConnection(connctionString);
+                var query = "SELECT case" +
+                           " when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '8.0%' then 'SQL Server 2000'" +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '9.0%' then 'SQL Server 2005'" +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '10.0%' then 'SQL Server 2008' " +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '10.5%' then 'SQL Server 2008 R2' " +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '11.0%' then 'SQL Server 2012' " +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '12.0%' then 'SQL Server 2014' " +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '13.0%' then 'SQL Server 2016' " +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "like '14.0%' then 'SQL Server 2017' " +
+                           "when CONVERT(sysname, SERVERPROPERTY('ProductVersion')) " +
+                           "> '14.0.9' then 'SQL Server 2019 RC' " +
+                           "else 'unknown' end as [version]";
+                var da = new SqlDataAdapter(query, cn);
+                var dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count <= 0) ver = 0;
+                var versionName = dt.Rows[0].ItemArray[0].ToString().Remove(0, 11);
+                ver = float.Parse(versionName);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                ver = 0;
+            }
+
+            return ver;
+
+        }
+    }
+}
---
+++ BackUpDLL/DatabaseAction.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Management.Smo;
+using PacketParser;
+using PacketParser.Services;
+
+namespace BackUpDLL
+{
+    public class DatabaseAction
+    {
+        public static void SetMultiUser(string connectionString)
+        {
+            try
+            {
+                var csb = new SqlConnectionStringBuilder(connectionString);
+                var serverConnectionString = connectionString.Replace("Initial Catalog=" + csb.InitialCatalog, "Initial Catalog=");
+                var cn = new SqlConnection(serverConnectionString);
+                var cmd = new SqlCommand("ALTER DATABASE [" + csb.InitialCatalog + "] SET Multi_USER", cn)
+                {
+                    CommandType = CommandType.Text
+                };
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+        }
+        public static string CreateFileName(string connectionString)
+        {
+            try
+            {
+                var FileName = "";
+                var csd = new SqlConnectionStringBuilder(connectionString);
+                var temp = Calendar.MiladiToShamsi(DateTime.Now);/*/*Settings.DateManagment.DateM.Date*/
+                temp = temp.Replace("/", "-");
+                temp = temp.Replace("\\", "-");
+                FileName = csd.InitialCatalog + "__" + temp + "__" + DateTime.Now.Hour/*Settings.DateManagment.DateM.Hour.ToString()*/ + "-" + DateTime.Now.Minute.ToString()/*Settings.DateManagment.DateM.Minute.ToString()*/ + ".NP";
+                FileName = FileName.Replace(" ", "");
+                var subPath = Path.Combine(Application.StartupPath, "TempBackUp"); // your code goes here
+                var exists = Directory.Exists(subPath);
+                if (!exists)
+                    Directory.CreateDirectory(subPath);
+                var fullPath = Path.Combine(subPath, FileName); // defultpath
+                return fullPath;
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                return "";
+            }
+        }
+        public static string BackupDB(string connectionString)
+        {
+            try
+            {
+                var fullPath = CreateFileName(connectionString);
+                return BackupDB(connectionString, fullPath);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                return "";
+            }
+        }
+
+        public static string BackupDB(string connectionString, string path)
+        {
+            var fullPath = path;
+            if (string.IsNullOrEmpty(fullPath))
+            {
+                fullPath = CreateFileName(connectionString);
+                path = fullPath;
+            }
+            try
+            {
+                try
+                {
+                    var fileName = "";
+                    var csbfilename = new SqlConnectionStringBuilder(connectionString);
+                    var temp1 = Calendar.MiladiToShamsi(DateTime.Now);
+                    temp1 = temp1.Replace("/", "-");
+                    temp1 = temp1.Replace("\\", "-");
+                    fileName = csbfilename.InitialCatalog + "__" + temp1 + "__" +
+                               DateTime.Now.Hour + "-" +
+                               DateTime.Now.Minute +
+                               ".NP";
+                    if (!path.ToLower().Contains(".bak") && !path.ToLower().Contains(".np"))
+                    {
+                        var exists = Directory.Exists(path);
+                        if (!exists)
+                            Directory.CreateDirectory(path);
+                        fullPath = Path.Combine(path, fileName);
+                    }
+                    var cn = new SqlConnection(connectionString);
+                    var csb = new SqlConnectionStringBuilder(connectionString);
+                    var cmd =
+                        new SqlCommand("Backup database [" + csb.InitialCatalog + "] to disk='" + fullPath + "'", cn)
+                        {
+                            CommandType = CommandType.Text
+                        };
+                    cmd.CommandTimeout *= 1000;
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    return path;
+                }
+                catch (SqlException ex2)
+                {
+                    MessageBox.Show($"حطا در تهیه نسخه پشتیبان اطلاعات \r\nحساب کاربری SQL دسترسی به مسیر نصب برنامه ندارد\r\n{ex2.Message}",
+                        "خطا", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RtlReading);
+                    WebErrorLog.ErrorInstence.StartErrorLog(ex2);
+                    return "";
+                }
+                catch (Exception ex)
+                {
+                    WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                    return "";
+                }
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                return "";
+            }
+        }
+        public static string ReStoreDB(string connectionString, string pathf, bool autoBackup)
+        {
+            var cn = new SqlConnection();
+            try
+            {
+                GC.Collect();
+                SqlConnection.ClearAllPools();
+
+                if (autoBackup)
+                {
+
+                    var dir = Application.StartupPath + "\\NovinPardazBackUp";
+                    if (!Directory.Exists(dir))
+                        Directory.CreateDirectory(dir);
+
+                    var file = Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location).Replace(".exe", "__");
+                    var d = Calendar.MiladiToShamsi(DateTime.Now).Replace("/", "_");
+                    d += "__" + DateTime.Now.Hour + " - " + DateTime.Now.Minute;
+                    file += d;
+
+                    var filepath = dir + "\\" + file + ".Bak";
+                    if (BackupDB(connectionString, filepath) == "")
+                        return $"حساب کاربری SQL دسترسی به مسیر نصب برنامه ندارد\r\nbackup Failed {connectionString}\r\nto address:{filepath}";
+                }
+                var csb = new SqlConnectionStringBuilder(connectionString);
+                var ServerConnectionString = connectionString.Replace("Initial Catalog=" + csb.InitialCatalog, "Initial Catalog=");
+                cn = new SqlConnection(ServerConnectionString) { ConnectionString = ServerConnectionString };
+                var cmd = new SqlCommand("RESTORE DATABASE [dbName] FROM  DISK = N'bkPath' WITH  FILE = 1,  MOVE N'novin' TO N'dbMDFPath',  MOVE N'novin_log' TO N'dbLDFPath',   NOUNLOAD,  REPLACE,  STATS = 5", cn);
+                //var dbInfo = new CLSServerDBs(connectionString);
+                //var backUpInfo = new DataBaseBackUpInfo(pathf, cn.ConnectionString.ToString());
+
+                cmd.CommandText = cmd.CommandText.Replace("dbName", csb.InitialCatalog);
+                cmd.CommandText = cmd.CommandText.Replace("bkPath", pathf);
+                //cmd.CommandText = cmd.CommandText.Replace("novin", backUpInfo.LogicalName);
+                //cmd.CommandText = cmd.CommandText.Replace("novin_log", backUpInfo.LogicalName + "_log.ldf");
+                //cmd.CommandText = cmd.CommandText.Replace("dbMDFPath", dbInfo.FileName);
+                //cmd.CommandText = cmd.CommandText.Replace("dbLDFPath", dbInfo.FileName.ToLower().Replace(".mdf", "_log.Ldf"));
+                cmd.CommandTimeout *= 1000;
+
+                cn.Open();
+                var temp = cmd.CommandText;
+                cmd.CommandText = "ALTER DATABASE [" + csb.InitialCatalog + "] SET SINGLE_USER WITH ROLLBACK IMMEDIATE";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = temp;
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = cmd.CommandText.Replace("dbName", csb.InitialCatalog);
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "ALTER DATABASE [" + csb.InitialCatalog + "] SET Multi_USER";
+                cmd.ExecuteNonQuery();
+                cn.Close();
+
+                SqlConnection.ClearAllPools();
+                Application.DoEvents();
+                SetMultiUser(connectionString);
+                return "";
+            }
+            catch (SqlException ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                cn.Close();
+                SetMultiUser(connectionString);
+                MessageBox.Show($"حساب کاربری SQL دسترسی به مسیر نصب برنامه ندارد\r\n{ex.Message }", "خطا", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RtlReading);
+                return ex.Message;
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                cn.Close();
+                SetMultiUser(connectionString);
+                return ex.Message;
+            }
+        }
+
+        //public static CreateDatabaseResult CreateDataBase(string DbName, string ServerConnectionString)
+        //{
+        //    var ret = new CreateDatabaseResult();
+        //    if (string.IsNullOrEmpty(DbName))
+        //    {
+        //        ret.Status = EnCreateDataBase.DatabaseExists;
+        //        ret.Result.AddReturnedValue(ReturnedState.Error, $"نام بانک اطلاعاتی خالی است.");
+        //        return ret;
+        //    }
+
+        //    if (string.IsNullOrEmpty(ServerConnectionString))
+        //    {
+        //        ret.Status = EnCreateDataBase.ServerConnectionStringError;
+        //        ret.Result.AddReturnedValue(ReturnedState.Error, $"رشته اتصال به سرور خالی است.");
+        //        return ret;
+        //    }
+
+        //    try
+        //    {
+        //        try
+        //        {
+        //            var serverCn = new SqlConnection(ServerConnectionString);
+        //            serverCn.Open();
+        //            serverCn.Close();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            ret.Status = EnCreateDataBase.ServerConnectionStringError;
+        //            ret.Result.AddReturnedValue(ReturnedState.Error, $"سرویس sql در دسترس نیست \r\nserver:{ServerConnectionString}\r\n{ex.Message}");
+        //            return ret;
+        //        }
+
+        //        SqlConnectionStringBuilder cbs = new SqlConnectionStringBuilder(ServerConnectionString);
+        //        cbs.InitialCatalog = "master";
+
+        //        if (ISExistDatabase(DbName, cbs.ConnectionString))
+        //        {
+        //            ret.Result.AddReturnedValue(ReturnedState.Error, $"بانک اطلاعاتی با نام {DbName} \r\nدر سرویس {ServerConnectionString}\r\n در حال حاضر وجود دارد.");
+        //            ret.Status = EnCreateDataBase.DatabaseExists;
+        //            return ret;
+        //        }
+
+        //        SqlConnection cn = new SqlConnection(cbs.ConnectionString);
+        //        SqlCommand cmd = new SqlCommand("", cn);
+        //        cmd.CommandType = CommandType.Text;
+        //        string DBPath = System.Windows.Forms.Application.StartupPath + "\\DataBase";
+
+        //        cmd.CommandText = "CREATE DATABASE [DBName]";
+        //        cmd.CommandText = cmd.CommandText.Replace("DBName", DbName);
+
+        //        var constr = cn.ConnectionString.Replace("master", DbName);
+        //        cn.Open();
+        //        cmd.ExecuteNonQuery();
+        //        cn.Close();
+
+        //        //return cn.ConnectionString.Replace("master", DbName);
+        //        ret.ConnectionString = constr;
+        //        ret.Status = EnCreateDataBase.Success;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        if (ex.Message.ToLower().Contains("because it already exists".ToLower()))
+        //            ret.Status = EnCreateDataBase.DatabaseExists;
+        //        else
+        //            WebErrorLog.ErrorInstence.StartErrorLog(ex);
+        //        ret.Result.AddReturnedValue(ex);
+        //    }
+        //    return ret;
+        //}
+
+        public static bool ISExistDatabase(string DBName, string CN)
+        {
+            try
+            {
+                int Exist = 0;
+                SqlConnection cn = new SqlConnection(CN);
+                SqlCommand cmd = new SqlCommand("SELECT case when exists (select 1 from sys.Databases where Name = @DbName) then 1 else 0 end as DbExists", cn);
+                cmd.Parameters.AddWithValue("@DbName", DBName);
+                cn.Open();
+                Exist = int.Parse(cmd.ExecuteScalar().ToString());
+                cn.Close();
+                if (Exist == 0)
+                    return false;
+                else return true;
+            }
+            catch (Exception e)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(e);
+                return false;
+            }
+        }
+    }
+}
